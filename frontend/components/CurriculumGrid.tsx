2b9067e8987a4f3e995cd302e36e509ae30cf829
Pichuelectrico <pichuelectrico@gmail.com>
2025-10-01 16:26:49 -0500

    Comment out upload button in CurriculumGrid
    The 'Cargar Malla' upload button has been commented out, likely to temporarily disable the upload functionality while retaining the code for future use.

--- frontend/components/CurriculumGrid.tsx
+++ frontend/components/CurriculumGrid.tsx
@@ -432,10 +432,10 @@ export default function CurriculumGrid() {
           onSelect={handleCurriculumSelect}
           onRequestCurriculum={() => setShowContact(true)}
         />
-        <Button onClick={() => setShowUpload(true)} variant="outline" className="dark:border-gray-600 dark:text-gray-200 dark:hover:bg-gray-700">
+        {/* <Button onClick={() => setShowUpload(true)} variant="outline" className="dark:border-gray-600 dark:text-gray-200 dark:hover:bg-gray-700">
           <Upload className="w-4 h-4 mr-2" />
           Cargar Malla
-        </Button>
+        </Button> */}
         <Button onClick={resetProgress} variant="outline" className="dark:border-gray-600 dark:text-gray-200 dark:hover:bg-gray-700">
           <RotateCcw className="w-4 h-4 mr-2" />
           Reiniciar Progreso
